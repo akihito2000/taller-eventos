b819d3d185065269b851d9a1ebc6cffb588f8bfe
akihito <akihito2000h@gmail.com>
2025-08-12 12:57:33 -0300

    solucion-click

--- eventos.js
+++ eventos.js
@@ -1,6 +1,7 @@
 
 
 let div = document.getElementsByTagName("div");
-    div[0].addEventListener("click", function(){
-        alert('Hola! Soy el div');
+    div[0].addEventListener("click", function(e) {
+    if (e.target.tagName !== "BUTTON") {
+        alert("Hola! Soy el div");}
     }) 
